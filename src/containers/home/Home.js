07ca12189b1b0d4f4db608bf5a7d2bfeed19dc1b
Sri <srinivasan.maha@gmail.com>
2022-02-10 01:40:49 -0600

    initial commit

---
+++ src/containers/home/Home.js
@@ -0,0 +1,42 @@
+const Home = () => {
+  return (
+    <>
+      <table style={{ border: "1" }}>
+        <thead>
+          <th>Name</th>
+          <th>DOB</th>
+          <th>Email</th>
+          <th>Position</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Srini</td>
+            <td>JAN-25-1981</td>
+            <td>Srini@Gmail.com</td>
+            <td>Manager</td>
+          </tr>
+          <tr>
+            <td>Srini1</td>
+            <td>JAN-25-1981</td>
+            <td>Srini@Gmail.com</td>
+            <td>Manager</td>
+          </tr>
+          <tr>
+            <td>Srini2</td>
+            <td>JAN-25-1981</td>
+            <td>Srini@Gmail.com</td>
+            <td>Manager</td>
+          </tr>
+          <tr>
+            <td>Srini3</td>
+            <td>JAN-25-1981</td>
+            <td>Srini@Gmail.com</td>
+            <td>Manager</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default Home;
